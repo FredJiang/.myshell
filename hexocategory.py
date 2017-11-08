1912f4001c54044037d9aacc775999e76687c57a
FredJiang <270130108@qq.com>
2017-11-08 11:29:35 +0800

    no message

--- hexocategory.py
+++ hexocategory.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python
 #coding:utf-8
 
+
+# kms
+# hexocategory.py 微信支付宝储值.md "id-微信支付宝储值-" | grep -v "    "
+
+
 import sys
 import re
 
 markdownFileName = sys.argv[1]
+
+headPrefix = ""
+if len(sys.argv) >= 3:
+    headPrefix = sys.argv[2]
 
 for line in open(markdownFileName):
     matchSharp = re.search(r"^#+", line)
@@ -12,7 +21,7 @@ for line in open(markdownFileName):
     if matchLine:
         title = re.sub("^#* +", "", matchLine.group(0))
         title = re.sub("\n", "", title)
-        result = "* [" + title + "]" + "(#" + re.sub(" ", "-", title) + ")"
+        result = "* [" + title + "]" + "(#" + headPrefix + re.sub(" ", "-", title) + ")"
         for x in range(0, len(matchSharp.group(0))):
             result = " " + result
         print(result)
