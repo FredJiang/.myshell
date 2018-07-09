08bdf5265abbb73c48115fac31f954d3e8ddcb9a
FredJiang <270130108@qq.com>
2018-07-09 17:35:12 +0800

    no message

--- hexocategory.py
+++ hexocategory.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python
 #coding:utf-8
 
+import logging
+# logging.basicConfig(level=logging.DEBUG,  
+#                     format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',  
+#                     datefmt='%Y-%m-%d %H:%M:%S')
 
 # kms
 # hexocategory.py 微信支付宝储值.md "id-微信支付宝储值-" | grep -v "    "
@@ -19,10 +23,13 @@ for line in open(markdownFileName):
     matchSharp = re.search(r"^#+", line)
     matchLine = re.search(r"^#([\s\S]*)", line)
     if matchLine:
+        logging.debug(matchLine.group(0))
         title = re.sub("^#* +", "", matchLine.group(0))
+        logging.debug(title)
         title = re.sub("\n", "", title)
-        result = "* [" + title + "]" + "(#" + headPrefix + re.sub(" ", "-", title) + ")"
+        logging.debug(title)
+        result = "* [" + title + "]" + "(#" + headPrefix + re.sub(" |\.", "-", title) + ")"
+        logging.debug(result)
         for x in range(0, len(matchSharp.group(0))):
             result = " " + result
         print(result)
-
